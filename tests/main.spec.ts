ff61b6cb61899b61d2f27b31935d4306f2c9192f
k.naydyuk <k.naydyuk@okko.tv>
2024-05-19 18:36:18 -0300

    Complete chapter 3 practice

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -27,4 +27,33 @@ describe("main.fc contract test", () => {
         const getDataResult = await contract.getData();
         expect(getDataResult.recent_sender.toString()).toBe(senderWallet.address.toString());
     });
+
+    it("Count sum of messages", async () => {
+        const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+
+        const blockchain = await Blockchain.create();
+
+        const contract = blockchain.openContract(
+            MainContract.createFromConfig({}, codeCell)
+        )
+
+        const senderWallet = await blockchain.treasury("sender");
+
+        const sendMessage1Result = await contract.sendInternalMessage(senderWallet.getSender(), toNano("0.05"), 20);
+        expect(sendMessage1Result.transactions).toHaveTransaction({
+            from: senderWallet.address,
+            to: contract.address,
+            success: true
+        });
+
+        const sendMessage2Result = await contract.sendInternalMessage(senderWallet.getSender(), toNano("0.1"), 30);
+        expect(sendMessage2Result.transactions).toHaveTransaction({
+            from: senderWallet.address,
+            to: contract.address,
+            success: true
+        });
+
+        const sum = await contract.getSum();
+        expect(sum).toBe(50);
+    });
 });
